efe8c685f638946577eece053ccff9e1498481a9
Greg Hackmann <ghackmann@google.com>
2013-01-15 09:32:52 -0800

    media: fimg2d: avoid bool type in userspace-facing structs
    Otherwise userspace (or this header) needs to #include <stdbool.h>;
    using a specific int type instead is arguably "more correct".
    
    Change-Id: Iabe65f7cecf6932ea88279fad35539b379c91921
    Signed-off-by: Greg Hackmann <ghackmann@google.com>

--- drivers/media/platform/exynos/fimg2d/fimg2d.h
+++ drivers/media/platform/exynos/fimg2d/fimg2d.h
@@ -359,7 +359,7 @@ struct fimg2d_scale {
 };
 
 struct fimg2d_clip {
-	bool enable;
+	__u32 enable;
 	int x1;
 	int y1;
 	int x2;	/* x1 + width */
@@ -410,7 +410,7 @@ struct fimg2d_image {
 struct fimg2d_param {
 	unsigned long solid_color;
 	unsigned char g_alpha;
-	bool dither;
+	__u32 dither;
 	enum rotation rotate;
 	enum premultiplied premult;
 	struct fimg2d_scale scaling;
@@ -500,7 +500,7 @@ struct fimg2d_control {
 	struct device *dev;
 	struct resource *mem;
 	void __iomem *regs;
-	bool boost;
+	__u32 boost;
 
 	atomic_t drvact;
 	atomic_t suspended;
