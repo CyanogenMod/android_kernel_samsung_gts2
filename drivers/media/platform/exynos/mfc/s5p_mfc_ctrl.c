89b608da772293f72776883f52964270afc7dfb3
Christopher N. Hesse <raymanfx@gmail.com>
2016-06-16 13:05:52 +0200

    media: exynos: mfc: Do not change clocks if a DRM context is running
    Turns out the previous patch did not handle all crash cases and we
    need a more aggressive approach to this.
    
    Change-Id: I855533212d407c7a9f67fc9add1185cd2cd580c0

--- drivers/media/platform/exynos/mfc/s5p_mfc_ctrl.c
+++ drivers/media/platform/exynos/mfc/s5p_mfc_ctrl.c
@@ -460,15 +460,8 @@ int mfc_init_hw(struct s5p_mfc_dev *dev, enum mfc_buf_usage_type buf_type)
 			s5p_mfc_clock_off(dev);
 			dev->curr_ctx_drm = curr_ctx_backup;
 			s5p_mfc_clock_on_with_base(dev, MFCBUF_NORMAL);
-		} else if (buf_type == MFCBUF_NORMAL) {
-			/* Must not change clocks if this is not the first instance */
-			if (curr_ctx_backup > 0) {
-				s5p_mfc_init_memctrl(dev, MFCBUF_DRM);
-			} else {
-				s5p_mfc_clock_off(dev);
-				dev->curr_ctx_drm = 1;
-				s5p_mfc_clock_on_with_base(dev, MFCBUF_DRM);
-			}
+		} else if (buf_type == MFCBUF_NORMAL && curr_ctx_backup) {
+			s5p_mfc_init_memctrl(dev, MFCBUF_DRM);
 		}
 	}
 #endif
